496ffd849e3508ea45cbc482baefec03d1cb78c2
walterlivingston <wblivingston18@gmail.com>
2021-08-11 14:01:52 -0500

    formatting tweaks

--- src/main.py
+++ src/main.py
@@ -1,35 +1,41 @@
 from cryptography.fernet import Fernet
 
-'''def write_key():
+"""def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)'''
+        key_file.write(key)"""
+
 
 def load_key():
     with open("key.key", "rb") as key_file:
         return key_file.read()
 
-#master_pwd = input("What is the master password: ")
-key = load_key() # + master_pwd.encode()
+
+# master_pwd = input("What is the master password: ")
+key = load_key()  # + master_pwd.encode()
 fer = Fernet(key)
 
+
 def view():
-    with open("passwords.txt", 'r') as f:
+    with open("passwords.txt", "r") as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User: ", user, "| Password: ", 
-                fer.decrypt(passw.encode()).decode())
+            print("User: ", user, "| Password: ", fer.decrypt(passw.encode()).decode())
+
 
 def add():
     name = input("Account Name: ")
     password = input("Password: ")
 
-    with open("passwords.txt", 'a') as f:
+    with open("passwords.txt", "a") as f:
         f.write(name + "|" + fer.encrypt(password.encode()).decode() + "\n")
 
+
 while True:
-    mode = input("Would you like to add a new password or view existing ones (view, add)? ")
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add)? "
+    )
     if mode == "q":
         break
     if mode == "view":
